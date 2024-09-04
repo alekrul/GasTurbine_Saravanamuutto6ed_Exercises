f566e6f73e68e96b86e5c8804ef810661430d6e6
Alexandre Krul <62573871+alekrul@users.noreply.github.com>
2024-09-04 19:20:16 -0300

    Update 5_4.py

--- 5_4.py
+++ 5_4.py
@@ -41,3 +41,50 @@ for i in range(n_stages):
 
 print(T_s)
 print(p_s)
+
+#(b) calculate rotational speed and length of the last stage rotot blade at inlet to the stage
+V1_s7 = 165 #[m/s]
+alpha1_s7 = radians(20) 
+work_done_factor = 0.83
+d_mean = 0.18 #[m]
+
+
+Va1_s7 = V1_s7*cos(alpha1_s7)
+V1u_s7 = V1_s7*sin(alpha1_s7)
+
+#assuming symm vel. diagram:
+W2_s7 = V1_s7
+beta2_s7 = alpha1_s7
+
+from sympy import symbols, Eq, solve
+
+U, tg_beta1 = symbols('U tg_beta1')
+
+#reaction 0.5 at mean:
+
+b1 = Va1_s7*tan(beta2_s7)
+b2 = deltaT_0s*cp/(work_done_factor*Va1_s7)
+
+eq1 = Eq(U - Va1_s7*tg_beta1,b1)
+eq2 = Eq(U*tg_beta1 - U*tan(alpha1_s7),b2)
+
+
+solutions = solve((eq1, eq2), (U, tg_beta1))
+
+U = [value[0] for value in solutions if value[0] > 0]
+tg_beta1 = [value[1] for value in solutions if value[1] > 0]
+
+N = U[0]/(pi*d_mean)
+
+print("Rotational speed: ", N)
+
+#length
+
+T_1 = T_s[n_stages-1] - (V1_s7**2)/(2*cp)
+p_1 = p_s[n_stages]*((T_1/T_s[n_stages])**aux)
+rho_1 = p_1*100/(T_1*R)
+A = m_dot/(rho_1*Va1_s7)
+l = A/(pi*d_mean)
+
+print("Length: ",l)
+
